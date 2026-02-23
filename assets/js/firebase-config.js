1267e35f2b1a17f69c836fe071178d08e1912209
PiyushKoli04 <piyushkoli2605@gmail.com>
2026-02-24 00:23:07 +0530

    Finally it worked!!

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBjhrdr3PpaSGv7KbACMTxAeJD33M3vqAg",
+  authDomain: "project-library-f676a.firebaseapp.com",
+  databaseURL: "https://project-library-f676a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "project-library-f676a",
+  storageBucket: "project-library-f676a.firebasestorage.app",
+  messagingSenderId: "122799963567",
+  appId: "1:122799963567:web:b036b99d40eceaf85e835a",
+  measurementId: "G-7WG05FMM2C"
+};
